b185ce4fc9e9240ea6ae5a1e27ab2ccdc76603e7
xushengfeng <xushengfeng_zg@163.com>
2026-01-04 18:27:14 +0800

    sh 优化样式

--- src/renderer/sh/sh.ts
+++ src/renderer/sh/sh.ts
@@ -1,18 +1,65 @@
-import { initDKH, input, textarea, txt, view } from "dkh-ui";
+import { initDKH, input, spacer, textarea, txt, view } from "dkh-ui";
 const path = require("node:path") as typeof import("node:path");
+const fs = require("node:fs") as typeof import("node:fs");
 const pty = require("node-pty") as typeof import("node-pty");
 const process = require("node:process") as typeof import("node:process");
 
 class Page {
-    env: Record<string, string> = {};
+    env: Record<string, string> = { ...process.env } as Record<string, string>;
     cwd: string = process.env.HOME || "";
 
     data: { startTime: number; finishTime: number; startCommand: string; outputRender: string }[] = [];
 
+    bin = new Set<string>();
+    innerCommand = new Set<string>(["cd"]);
+
+    private inputPromptSymbol = { cursor: Symbol("cursor"), spacer: Symbol("spacer"), cwd: Symbol("cwd") };
+    private inputPrompt: (string | symbol)[][] = [];
+
     mainEl = view();
     historyEl = view("y").addInto(this.mainEl);
-    inputCommandEl = input().addInto(this.mainEl);
-    constructor() {
+    private inputAreaEl = view().addInto(this.mainEl);
+    private inputPromptEl = view().addInto(this.inputAreaEl);
+    private inputCommandEl = input().addInto(this.inputAreaEl);
+    constructor(op?: { inputPrompt?: string }) {
+        const finish = () => {
+            const binDir = new Set(["/bin", "/usr/bin", "/usr/local/bin"]);
+            for (const dir of process.env.PATH?.split(":") || []) binDir.add(dir);
+            this.bin.clear();
+            for (const dir of binDir) {
+                try {
+                    const files = fs.readdirSync(dir);
+                    for (const file of files) {
+                        this.bin.add(file);
+                    }
+                } catch {
+                    // ignore
+                }
+            }
+
+            const promptRendered = this.inputPrompt.map((l) => {
+                return l
+                    .map((i) => {
+                        if (i === this.inputPromptSymbol.cursor) {
+                            return this.inputCommandEl;
+                        }
+                        if (i === this.inputPromptSymbol.spacer) {
+                            return spacer();
+                        }
+                        if (i === this.inputPromptSymbol.cwd) {
+                            return this.cwd;
+                        }
+                        return i;
+                    })
+                    .filter((i) => typeof i !== "symbol");
+            });
+
+            this.inputPromptEl.clear().add(promptRendered.map((l) => view("x").add(l)));
+
+            this.inputCommandEl.attr({ disabled: false });
+            this.inputCommandEl.el.focus();
+        };
+
         this.inputCommandEl.on("change", () => {
             const command = this.inputCommandEl.gv;
             this.inputCommandEl.sv("");
@@ -24,16 +71,24 @@ class Page {
 
             historyEl.addInto(this.historyEl);
 
-            const finish = () => {
-                this.inputCommandEl.attr({ disabled: false });
-                this.inputCommandEl.el.focus();
-            };
-
             const com = command.split(" ");
             if (com.length === 0) {
                 finish();
                 return;
             }
+
+            const finish2 = () => {
+                dataItem.finishTime = Date.now();
+                dataItem.outputRender = outputEl.gv;
+                finish();
+            };
+
+            const dataItem: (typeof this.data)[0] = {
+                startTime: Date.now(),
+                finishTime: 0,
+                startCommand: command,
+                outputRender: "",
+            };
 
             if (com[0] === "cd") {
                 if (com.length > 1) {
@@ -43,7 +98,13 @@ class Page {
                 } else {
                     this.cwd = process.env.HOME || "";
                 }
-                finish();
+                finish2();
+                return;
+            }
+
+            if (!this.allCommands().has(com[0])) {
+                outputEl.sv(`Command not found: ${com[0]}\n`);
+                finish2();
                 return;
             }
 
@@ -58,17 +119,62 @@ class Page {
             });
 
             ptyProcess.onData((data) => {
+                // todo parse ANSI escape codes
+                // todo 数据有时不完整
+                // https://github.com/microsoft/node-pty/issues/72
+                // https://github.com/microsoft/node-pty/issues/85
                 outputEl.sv(outputEl.gv + data);
             });
             ptyProcess.onExit(() => {
-                finish();
+                finish2();
             });
         });
+
+        if (op?.inputPrompt) {
+            this.setInputPrompt(op.inputPrompt);
+        }
+        finish();
+    }
+
+    private allCommands() {
+        return this.innerCommand.union(this.bin);
+    }
+
+    setInputPrompt(prompt: string) {
+        const promptRendered = prompt.split("\n").map((line) => {
+            const xsymbol = this.inputPromptSymbol;
+            // todo 解析，转义
+            const l: (string | (typeof xsymbol)[keyof typeof xsymbol])[] = [""];
+            xx: for (let i = 0; i < line.length; ) {
+                for (const [k, v] of Object.entries(xsymbol)) {
+                    if (line.startsWith(`\${${k}}`, i)) {
+                        l.push(v);
+                        i += `\${${k}}`.length;
+                        continue xx;
+                    }
+                }
+                if (typeof l.at(-1) === "symbol") {
+                    l.push("");
+                }
+                l[l.length - 1] = (l[l.length - 1] as string) + line[i];
+                i += 1;
+            }
+            return l;
+        });
+        if (promptRendered.flat().find((i) => i === this.inputPromptSymbol.cursor) === undefined) {
+            this.inputPrompt = [[this.inputPromptSymbol.cursor]];
+        } else this.inputPrompt = promptRendered;
     }
 }
 
 initDKH({
     pureStyle: true,
 });
-const p1 = new Page();
-p1.mainEl.addInto();
+const p1 = new Page({
+    inputPrompt: "<${cwd}${spacer}>\n$ ${cursor}",
+});
+p1.mainEl
+    .style({
+        fontFamily: "'FiraCode Nerd Font Mono'",
+    })
+    .addInto();
